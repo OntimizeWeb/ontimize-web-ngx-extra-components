0b3421fd028a261774d63957f7202c7fe6c0dd59
Daniel <daniel.grana@imatia.com>
2025-05-09 09:10:48 +0200

    chore: fixt sonar issues

--- projects/ontimize-web-ngx-extra-components/src/lib/util/constants.ts
+++ projects/ontimize-web-ngx-extra-components/src/lib/util/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
-  public static DEFAULT_WIDTH_DIALOG = '70%';
-  public static DEFAULT_HEIGHT_DIALOG = '90%';
-  public static DEFAULT_WIDTH_COLUMN_STYLE = 85;
-  public static DEFAULT_ALIGNMENT_COLUMN_STYLE: 'center' | 'right' | 'left' = 'left';
+  public static readonly DEFAULT_WIDTH_DIALOG = '70%';
+  public static readonly DEFAULT_HEIGHT_DIALOG = '90%';
+  public static readonly DEFAULT_WIDTH_COLUMN_STYLE = 85;
+  public static readonly DEFAULT_ALIGNMENT_COLUMN_STYLE: 'center' | 'right' | 'left' = 'left';
 }
--- projects/ontimize-web-ngx-extra-components/src/lib/util/utils.ts
+++ projects/ontimize-web-ngx-extra-components/src/lib/util/utils.ts
@@ -2,12 +2,11 @@ import { MatDialog } from "@angular/material/dialog";
 import { Constants } from "./constants";
 
 export class Utils {
-  static setFullscreenDialog(fullscreen: boolean, dialogRef: any): void {
-    if (!fullscreen) {
-      dialogRef.updateSize("100%", "100%");
-    } else {
-      dialogRef.updateSize(Constants.DEFAULT_WIDTH_DIALOG, Constants.DEFAULT_HEIGHT_DIALOG);
-    }
+  static setDefaultDialogSize(dialogRef: any): void {
+    dialogRef.updateSize(Constants.DEFAULT_WIDTH_DIALOG, Constants.DEFAULT_HEIGHT_DIALOG);
+  }
+  static setFullscreenDialogSize(dialogRef: any): void {
+    dialogRef.updateSize("100%", "100%");
   }
   static cloneObject(data: Object) {
     return JSON.parse(JSON.stringify(data));
