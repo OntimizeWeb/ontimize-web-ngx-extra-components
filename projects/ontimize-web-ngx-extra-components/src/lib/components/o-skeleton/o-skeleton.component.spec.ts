90f2b093ba1a59d1c8f5885f7e919ef6c5a74355
Noelia Prol Carballo <noelia.prol@imatia.com>
2025-12-04 17:10:02 +0100

    added app_config in sheleton test

--- projects/ontimize-web-ngx-extra-components/src/lib/components/o-skeleton/o-skeleton.component.spec.ts
+++ projects/ontimize-web-ngx-extra-components/src/lib/components/o-skeleton/o-skeleton.component.spec.ts
@@ -1,8 +1,17 @@
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OntimizeWebModule, APP_CONFIG, Config } from 'ontimize-web-ngx';
 
 import { OSkeletonComponent } from './o-skeleton.component';
 import { OExtraComponentsModule } from '../../ontimize-web-ngx-extra-components.module';
+
+const TEST_CONFIG: Config = {
+  apiEndpoint: '',
+  uuid: 'test-app',
+  title: 'test',
+  locale: 'en',
+  applicationLocales: ['en']
+};
 
 describe('OSkeletonComponent', () => {
   let component: OSkeletonComponent;
@@ -12,10 +21,11 @@ describe('OSkeletonComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         OExtraComponentsModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        OntimizeWebModule
       ],
       providers: [
-
+        { provide: APP_CONFIG, useValue: TEST_CONFIG }
       ]
     })
     .compileComponents();
