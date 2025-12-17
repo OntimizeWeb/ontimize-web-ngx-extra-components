7fba8b814de3a4d9690e88667bdda86eba2cf03e
Noelia Prol Carballo <noelia.prol@imatia.com>
2025-12-17 17:20:47 +0100

    fix: expose directives

--- projects/ontimize-web-ngx-extra-components/src/public-api.ts
+++ projects/ontimize-web-ngx-extra-components/src/public-api.ts
@@ -3,6 +3,10 @@
  */
 export * from './lib/ontimize-web-ngx-extra-components.module';
 export * from './lib/components/o-data-view/o-data-view.component';
+
+export * from './lib/components/o-data-view/o-data-view-grid-item.directive';
+export * from './lib/components/o-data-view/o-data-view-list-item.directive';
+export * from './lib/components/o-data-view/o-data-view.types';
 // export * from './lib/components/index';
 // export * from './lib/services/index';
 // export * from './lib/types/index';
